b9466cd09d31f14cb04a2e8571a09d756373a466
Stuart Demmer <dstuart99@gmail.com>
2023-03-25 14:34:27 +0200

    feat(frontend): :sparkles: created react app with vite and tailwind

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="">
+      <h1>Profile</h1>
+      <p className="">Welcome!</p>
+    </div>
+  );
+}
+
+export default Home;
