dff847f92f14df638d973462dec174890216e5a7
Stuart Demmer <dstuart99@gmail.com>
2023-03-29 14:31:32 +0200

    fix(backend): :bug: fix on CORS_ALLOWED_ORIGINS split default

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -74,10 +74,10 @@ MIDDLEWARE = [
 #     'http//:127.0.0.1:5173',
 # )
 
-CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173"
-]).split(",")
+CORS_ALLOWED_ORIGINS = os.getenv(
+    "CORS_ALLOWED_ORIGINS",
+    "http://localhost:5173, http://127.0.0.1:5173"
+).split(",")
 
 ROOT_URLCONF = 'CoreRoot.urls'
 
