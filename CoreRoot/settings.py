a58ac18292f770020ff964e2fa52c1da75f22be7
Stuart Demmer <dstuart99@gmail.com>
2023-03-31 13:50:05 +0200

    fix(backend): :bug: final fix

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -164,14 +164,12 @@ if USE_S3:
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = "django-react-backend-static"
     AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}'
+    AWS_S3_CLOUDFRONT_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN')
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # s3 static settings
     AWS_LOCATION = 'core_staticfiles'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    AWS_STORAGE_BUCKET_NAME = "django-react-backend-static"
-    AWS_S3_CUSTOM_DOMAIN = '%s.us-east-2.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    STATIC_URL = f'https://{AWS_S3_CLOUDFRONT_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 else:
     STATIC_URL = '/core_staticfiles/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'core_staticfiles')
